644504bdf85692915720eccc925fddb25839b694
Brandi <hello@brandicameron.com>
2020-05-04 16:48:38 -0500

    took the array out of the js code

--- js/main.js
+++ js/main.js
@@ -1,8 +1,26 @@
+//
+//let correctBtn = Array.prototype.slice.call(document.querySelectorAll('.correct-button'));
+//let correctBtnMulti = Array.prototype.slice.call(document.querySelectorAll('.correct-button-multi'));
+//let correctAnswerCheck = Array.prototype.slice.call(document.querySelectorAll('.correct-answer'));
+//let correctAnswerCheckMulti = Array.prototype.slice.call(document.querySelectorAll('.correct-answer-multi'));
+//	
+//	correctBtn.forEach(function(btn, index) {
+//		btn.addEventListener('click', function() {
+//			correctAnswerCheck[index].classList.add('correct-clicked');
+//		});
+//	});
+//
+//	correctBtnMulti.forEach(function(btn, index) {
+//		btn.addEventListener('click', function() {
+//			correctAnswerCheckMulti[index].classList.add('correct-clicked-multi');
+//		});
+//	});
 
-let correctBtn = Array.prototype.slice.call(document.querySelectorAll('.correct-button'));
-let correctBtnMulti = Array.prototype.slice.call(document.querySelectorAll('.correct-button-multi'));
-let correctAnswerCheck = Array.prototype.slice.call(document.querySelectorAll('.correct-answer'));
-let correctAnswerCheckMulti = Array.prototype.slice.call(document.querySelectorAll('.correct-answer-multi'));
+
+let correctBtn = document.querySelectorAll('.correct-button');
+let correctBtnMulti = document.querySelectorAll('.correct-button-multi');
+let correctAnswerCheck = document.querySelectorAll('.correct-answer');
+let correctAnswerCheckMulti = document.querySelectorAll('.correct-answer-multi');
 	
 	correctBtn.forEach(function(btn, index) {
 		btn.addEventListener('click', function() {
@@ -15,4 +33,3 @@ let correctAnswerCheckMulti = Array.prototype.slice.call(document.querySelectorA
 			correctAnswerCheckMulti[index].classList.add('correct-clicked-multi');
 		});
 	});
-
