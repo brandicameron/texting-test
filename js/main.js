3ef8537e10a8206013c2bea0cbc793600f46af5d
Brandi <hello@brandicameron.com>
2020-05-05 08:59:50 -0500

    added animations and made frame corners smaller on mobile / also cleaned up the classes a bit

--- js/main.js
+++ js/main.js
@@ -1,23 +1,6 @@
-//
-//let correctBtn = Array.prototype.slice.call(document.querySelectorAll('.correct-button'));
-//let correctBtnMulti = Array.prototype.slice.call(document.querySelectorAll('.correct-button-multi'));
-//let correctAnswerCheck = Array.prototype.slice.call(document.querySelectorAll('.correct-answer'));
-//let correctAnswerCheckMulti = Array.prototype.slice.call(document.querySelectorAll('.correct-answer-multi'));
-//	
-//	correctBtn.forEach(function(btn, index) {
-//		btn.addEventListener('click', function() {
-//			correctAnswerCheck[index].classList.add('correct-clicked');
-//		});
-//	});
-//
-//	correctBtnMulti.forEach(function(btn, index) {
-//		btn.addEventListener('click', function() {
-//			correctAnswerCheckMulti[index].classList.add('correct-clicked-multi');
-//		});
-//	});
 
-
-let correctBtn = document.querySelectorAll('.correct-button');
+//Adds green checkmark when correct answer is selected
+let correctBtn = document.querySelectorAll('.correct');
 let correctBtnMulti = document.querySelectorAll('.correct-button-multi');
 let correctAnswerCheck = document.querySelectorAll('.correct-answer');
 let correctAnswerCheckMulti = document.querySelectorAll('.correct-answer-multi');
@@ -33,3 +16,40 @@ let correctAnswerCheckMulti = document.querySelectorAll('.correct-answer-multi')
 			correctAnswerCheckMulti[index].classList.add('correct-clicked-multi');
 		});
 	});
+
+
+
+//Scroll animations
+let scroll = window.requestAnimationFrame ||
+	function (callback) {
+		window.setTimeout(callback, 1000 / 60)
+	};
+
+let animationTarget = document.querySelectorAll('.animate');
+
+function loop() {
+	animationTarget.forEach(function (element) {
+		if (isElementInViewport(element)) {
+			element.classList.add('fancy');
+		} else {
+			element.classList.remove('fancy');
+		}
+	});
+	scroll(loop);
+}
+
+loop();
+
+
+// Helper function from: http://stackoverflow.com/a/7557433/274826
+function isElementInViewport(el) {
+	var rect = el.getBoundingClientRect();
+	return (
+		(rect.top <= 0 &&
+			rect.bottom >= 0) ||
+		(rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+			rect.top <= (window.innerHeight || document.documentElement.clientHeight)) ||
+		(rect.top >= 0 &&
+			rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+	);
+}
