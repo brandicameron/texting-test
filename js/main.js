286583340b4a44aa0c9a28140b2c4c804f0343e9
Brandi <hello@brandicameron.com>
2020-05-06 13:42:01 -0500

    playing with scroll snap

--- js/main.js
+++ js/main.js
@@ -41,37 +41,38 @@ correctMulti.forEach(function (btn, index) {
 });
 
 
+
+
 //Scroll animations
-let scroll = window.requestAnimationFrame ||
-	function (callback) {
-		window.setTimeout(callback, 1000 / 60)
-	};
-
-let animationTarget = document.querySelectorAll('.animate');
-
-function loop() {
-	animationTarget.forEach(function (element) {
-		if (isElementInViewport(element)) {
-			element.classList.add('fancy');
-		} else {
-			element.classList.remove('fancy');
-		}
-	});
-	scroll(loop);
-}
-
-loop();
-
-
+//let scroll = window.requestAnimationFrame ||
+//	function (callback) {
+//		window.setTimeout(callback, 1000 / 60)
+//	};
+//
+//let animationTarget = document.querySelectorAll('.animate');
+//
+//function loop() {
+//	animationTarget.forEach(function (element) {
+//		if (isElementInViewport(element)) {
+//			element.classList.add('fancy');
+//		} else {
+//			element.classList.remove('fancy');
+//		}
+//	});
+//	scroll(loop);
+//}
+//
+//loop();
+//
+//function isElementInViewport(el) {
+//	var rect = el.getBoundingClientRect();
+//	return (
+//		(rect.top <= 0 &&
+//			rect.bottom >= 0) ||
+//		(rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+//			rect.top <= (window.innerHeight || document.documentElement.clientHeight)) ||
+//		(rect.top >= 0 &&
+//			rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+//	);
+//}
 // Helper function from: http://stackoverflow.com/a/7557433/274826 for scroll animations
-function isElementInViewport(el) {
-	var rect = el.getBoundingClientRect();
-	return (
-		(rect.top <= 0 &&
-			rect.bottom >= 0) ||
-		(rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
-			rect.top <= (window.innerHeight || document.documentElement.clientHeight)) ||
-		(rect.top >= 0 &&
-			rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
-	);
-}
