772efaf83dd1c8df38c7ea8fbcc54863fe94ce30
Brandi Cameron <BrandiMacBook@Brandi-Camerons-MacBook-Pro.local>
2020-08-16 17:57:56 -0500

    started over

--- js/main.js
+++ js/main.js
@@ -1,19 +1,21 @@
 //Applying user's name to questions and lift intro section when "Begin Test" is clicked
 
-const beginTestBtn = document.getElementById('begin-test');
+const beginTestBtn = document.getElementById('begin-test-intro');
 
-function captureUserName() {
+function captureUserInfo() {
 	let applyUserName = document.getElementById("userName").value;
 	let findSpans = document.getElementsByClassName("user-name");
 	let loopSpans = findSpans.length;
 	for (i = 0; i < loopSpans; i++) {
-		findSpans[i].innerHTML = applyUserName;
+		findSpans[i].textContent = applyUserName;
 	};
-	document.getElementById("section1").classList.add('lift-intro');
-	document.getElementById("body").classList.remove('no-overflow');
+	document.getElementById("intro-section").classList.add('lift-intro');
+	//if player is in the middle of answering questions and refreshes to go back to the beginning, this will reset to the instruction page after the intro page
+	window.location.href = '#player-instructions';
+//	document.getElementById("body").classList.remove('overflow');
 };
 
-beginTestBtn.addEventListener('click', captureUserName);
+beginTestBtn.addEventListener('click', captureUserInfo);
 
 
 //Make submit button work with enter key press also
